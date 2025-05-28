55e2a9fcf2c627f94f49ad6f8a8d7eae88aa106a
Toby Earle <toby_earle@yahoo.com>
2025-05-28 13:26:53 -0400

    added values and ran code

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string vehicleMake = "jeep";
+            int year = 2015;
+            double motorSize = 6.1;
+            decimal price = 45000.99m;
+            char vehicleClass = 'a';
+            bool isTurbocharged = true;
+            Console.WriteLine($"vehicle make; {vehicleMake} \nyear: {year}\nmotor size: {motorSize}\ncost: {price}\nvehicle class: {vehicleClass}\nis turbocharged: {isTurbocharged} ");
+            
         }
     }
 }
